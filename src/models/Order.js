578c615367209a55dbe4f6d1e5ce9a424af2c77f
Vicky Soni <murlimanoharsoni4@gmail.com>
2022-11-22 14:26:23 +0530

    created Order Model

---
+++ src/models/Order.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const ObjectId = Schema.Types.ObjectId;
+
+const User = require("./User");
+
+const orderSchema = new Schema(
+    {
+        userId: { type: ObjectId, ref: User, required: true },
+        addId: { type: ObjectId, required: true }, //store address id from user add array
+        order: [productSchema],
+    },
+    { timestamps: true }
+);
+
+const productSchema = new Schema({
+    prodType: {
+        type: String,
+        enum: [
+            "shirts",
+            "tshirts",
+            "trousers",
+            "jeans",
+            "boxers",
+            "joggers",
+            "others",
+        ],
+        required: true,
+    },
+    serviceCharge: {
+        wash: { type: Number, required: true },
+        iron: { type: Number, required: true },
+        dryClean: { type: Number, required: true },
+        bleaching: { type: Number, required: true },
+    },
+    quantity: { type: Number, required: true, default: 0 },
+    washType: {
+        wash: { type: Boolean, required: true, default: false },
+        iron: { type: Boolean, required: true, default: false },
+        dryClean: { type: Boolean, required: true, default: false },
+        bleaching: { type: Boolean, required: true, default: false },
+    },
+});
+
+const Order = mongoose.model('orders', orderSchema);
+
+module.exports = Order;
