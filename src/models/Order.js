6d379e805d439a995f125810649c76af29a61774
Vicky Soni <murlimanoharsoni4@gmail.com>
2022-11-23 19:51:22 +0530

    corrected Order and User model
    also corrected the address field in order now storing as an object rather than a addId

--- src/models/Order.js
+++ src/models/Order.js
@@ -5,39 +5,17 @@ const ObjectId = Schema.Types.ObjectId;
 const User = require("./User");
 const Store = require("./Store");
 
-const orderSchema = new Schema(
-    {
-        userId: { type: ObjectId, ref: User, required: true },
-        addId: { type: ObjectId, required: true }, //store address id from user add array
-        storeAdd: { type: ObjectId, ref: Store, required: true },
-        order: [productSchema],
-        status: {
-            type: String,
-            enum: [
-                "Ready to pickup",
-                "In washing",
-                "Canceled",
-                "Ready to deliver",
-                "In ironing",
-            ],
-            required: true,
-            default: "Ready to pickup",
-        },
-    },
-    { timestamps: true }
-);
-
 const productSchema = new Schema({
     prodType: {
         type: String,
         //enum: [
-            //"shirts",
-            //"tshirts",
-            //"trousers",
-            //"jeans",
-            //"boxers",
-            //"joggers",
-            //"others",
+        //"shirts",
+        //"tshirts",
+        //"trousers",
+        //"jeans",
+        //"boxers",
+        //"joggers",
+        //"others",
         //],
         required: true,
     },
@@ -55,6 +33,34 @@ const productSchema = new Schema({
         bleaching: { type: Boolean, required: true, default: false },
     },
 });
+
+const orderSchema = new Schema(
+    {
+        userId: { type: ObjectId, ref: User, required: true },
+        add: {
+            addName: { type: String, required: true, default: "Home" },
+            address: { type: String, required: true },
+            pincode: { type: Number, required: true },
+            state: { type: String, required: true },
+            district: { type: String, required: true },
+        },
+        storeId: { type: ObjectId, ref: Store, required: true },
+        order: [productSchema],
+        status: {
+            type: String,
+            enum: [
+                "Ready to pickup",
+                "In washing",
+                "Canceled",
+                "Ready to deliver",
+                "In ironing",
+            ],
+            required: true,
+            default: "Ready to pickup",
+        },
+    },
+    { timestamps: true }
+);
 
 const Order = mongoose.model("orders", orderSchema);
 
