3481ec03ddbe33cc05416a1b550a0e1b7ac498c7
Vicky Soni <murlimanoharsoni4@gmail.com>
2022-11-23 19:49:59 +0530

    added orders route

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const ordersRoute = require("./routes/orders");
 
 //body-parsers
 app.use(express.json());
@@ -8,5 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (_req, res) => {
     res.send("<h1>Hello World</h1>");
 });
+
+app.use("/orders", ordersRoute);
 
 module.exports = app;
