413d2cdee8ae8af3426d1580b3acb70340a85d4f
Vicky Soni <murlimanoharsoni4@gmail.com>
2022-11-24 01:32:42 +0530

    added the register endpoint

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const ordersRoute = require("./routes/orders");
 const productTypeRoute = require("./routes/productType");
+const registerRoute = require("./routes/register");
 
 //body-parsers
 app.use(express.json());
@@ -11,6 +12,8 @@ app.get("/", (_req, res) => {
     res.send("<h1>Hello World</h1>");
 
 });
+
+app.use("/register", registerRoute);
 
 app.use("/orders", ordersRoute);
 app.use("/products", productTypeRoute);
