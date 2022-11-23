0eac7162bdcfcc54d29f369f78200e2728e3c55e
Vicky Soni <murlimanoharsoni4@gmail.com>
2022-11-23 20:54:11 +0530

    refactored the get orders endpoint

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -8,19 +8,21 @@ router.post("/", async (req, res) => {
         //const {_id: userId} = req.user;
         //const userId = "some id";
 
-        const {userId, add, storeId, order} = req.body;
+        const { userId, add, storeId, order } = req.body;
 
         const result = await Order.create({
-            userId, add, storeId, order
+            userId,
+            add,
+            storeId,
+            order,
         });
-        
-        await result.populate('storeId');
-        
+
+        await result.populate("storeId");
 
         res.json({
             status: "success",
             message: "order succefully created",
-            order: result
+            order: result,
         });
     } catch (err) {
         console.log("/orders endpoint POST err:: ");
@@ -28,26 +30,36 @@ router.post("/", async (req, res) => {
         res.status(500).json({
             status: "failed",
             message: "server error during creating the order",
-            error: err.message
+            error: err.message,
         });
     }
 });
 
-router.get("/orders", (req, res)=> {
-
+router.get("/", (_req, res) => {
     try {
-
-        Orders.find({}).then((data)=>{
-            res.status(200).send(data)
-        }).catch((err)=>{
-            res.status(400).send(err);
-        })
-
-    } catch(err) {
-        // console.log(err)
-        res.status(400).send("Unauthorize user")
-    }    
-   
+        //search using UserId
+        Order.find({})
+            .then((data) => {
+                res.status(200).json({
+                    status: "success",
+                    message: "successfully fetched all orders of user",
+                    orders: data,
+                });
+            })
+            .catch((err) => {
+                console.log("Err while fetching the orders:: ");
+                console.dir(err);
+                res.status(400).json({
+                    status: "failed",
+                    message: "error while fetching orders",
+                    error: err.message,
+                });
+            });
+    } catch (err) {
+        //TODO change after implementing middleware for the auth
+        //console.log(err)
+        res.status(400).send("Unauthorize user");
+    }
 });
 
 module.exports = router;
