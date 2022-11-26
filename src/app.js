dc36f33f401537b8750077a8ce2a3368c98b4ae8
Vicky Soni <murlimanoharsoni4@gmail.com>
2022-11-27 02:32:00 +0530

    added /stores endpoint

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const ordersRoute = require("./routes/orders");
 const productTypeRoute = require("./routes/productType");
+const storeRoute = require("./routes/stores");
 const registerRoute = require("./routes/register");
 const loginRoute = require("./routes/login");
 const authenticate = require("./routes/utils/auth");
@@ -24,5 +25,6 @@ app.use("/orders", authenticate);
 app.use("/orders", ordersRoute);
 
 app.use("/products", productTypeRoute);
+app.use("/stores", storeRoute);
 
 module.exports = app;
